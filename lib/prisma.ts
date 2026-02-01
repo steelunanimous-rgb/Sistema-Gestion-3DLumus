8238ec8d9b5ad1c4ef7f9e33ad280b97e99083a7
steelunanimous-rgb <steelunanimous@gmail.com>
2026-02-01 16:14:55 -0600

    eror
    rrr

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,19 @@
 import { PrismaClient } from '@prisma/client'
 
+const prismaClientOptions: any = {}
+
+if (process.env.DATABASE_URL) {
+    prismaClientOptions.datasources = {
+        db: {
+            url: process.env.DATABASE_URL,
+        },
+    }
+}
+
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient(prismaClientOptions)
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
